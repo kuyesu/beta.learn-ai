333b11039ac8725cefdbf419970f0e5097db6388
kuyesu <kuyesu22@gmail.com>
2023-10-01 20:29:21 +0300

    remove s link

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -60,7 +60,7 @@ export default async function Navbar() {
                 <div
                   className={clsx(
                     'rounded-full bg-gradient-to-tl from-pink-600 to-[#A02BFE] via-[#02FCF1] p-[1px] subpixel-antialiased',
-                    s.link
+                   
                   )}
                 >
                   <p className="animate-border-pulse py group relative flex w-max cursor-pointer items-center gap-2 rounded-full bg-black px-4 py-2 text-xs text-white">
